b2cef7700a09660ff8a79ae587480680bef65a88
trsqxyz <trsqxyz@gmail.com>
2015-12-13 01:20:49 +0900

    Update: setup.py ver = 1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = "glitch",
-    version = "1.3",
+    version = "1.4",
     description = "glitch jpg files",
     license = 'MIT',
     author = "trsqxyz",
